9e32ced7e12f6354ee9f4f457befb4137b542bf0
America Ocampo <americavocampo@gmail.com>
2022-04-04 17:02:11 -0700

    first commit

---
+++ Fundamentals/YourOwn/YourOwn.java
@@ -0,0 +1,8 @@
+public class YourOwn {
+    public static void main(String[] args){
+        System.out.println("My name is America");
+        System.out.println("I feel like I am 80 years old");
+        System.out.println("My hometown is Los Angeles, CA");
+    }
+}
+
